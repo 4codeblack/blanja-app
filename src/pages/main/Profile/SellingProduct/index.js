ba969ea91eb883969e34d052d5bfad9e3a8bed8e
vigarp <vigar.8421@outlook.com>
2022-01-22 22:01:39 +0700

    membuat form validasi page store profile dan selling product

--- src/pages/main/Profile/SellingProduct/index.js
+++ src/pages/main/Profile/SellingProduct/index.js
@@ -1,8 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../../../../components/base/Button';
 import Input from '../../../../components/base/Input';
 
 const SellingProduct = () => {
+  const [loading, setLoading] = useState(false)
+  const [formError, setFormError] = useState({})
+
+  const [form, setForm] = useState({
+    inventory: '',
+    unitprice: '',
+    unitbuah: '',
+    unitstock: '',
+    description: ''
+  })
+
+  const handleChangeForm = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: [e.target.value]
+    })
+  }
+
+  const validateForm = (values) => {
+    const errors = {};
+    const textStore = values.description.toString()
+    
+    if (!values.inventory) {
+      errors.inventory = "Please input product name";
+    }
+    if (!values.unitprice) {
+      errors.unitprice = "Please input price of goods";
+    }
+    if (!values.unitbuah) {
+      errors.unitbuah = "Please input quantity of goods";
+    }
+    if (!values.unitstock) {
+      errors.unitstock = "Please select type of goods";
+    }
+    if (!values.description) {
+      errors.description = "Description of goods is required";
+    } else if (textStore.length < 120) {
+      errors.description = "Description atleast must be more than 120 characters";
+    }
+    return errors;
+  }
+
+  const handleClickForm = (resultValidate) => {
+    if (Object.keys(resultValidate).length === 0) {
+      setLoading(true)
+      console.log("dari form", form)
+      alert("oke")
+    }
+  }
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    const resultValidate = validateForm(form)
+    setFormError(resultValidate)
+    handleClickForm(resultValidate)
+    console.log(resultValidate)
+    console.log("isi juga dari", form)
+  }
   return (
     <div className="outer bg-light w-75">
       <div className="inner m-5 bg-white rounded-3">
@@ -13,9 +71,13 @@ const SellingProduct = () => {
           </div>
           <div className="lower px-5 pt-3">
             <div className="text-muted">Name of goods</div>
+            <div className='text-danger'>{formError.inventory}</div>
             <Input
               className="w-50 border py-2 mt-3 mb-5 rounded-3 border-2"
               type="text"
+              name="inventory"
+              onChange={handleChangeForm}
+              value={form.inventory}
               placeholder="input name of goods" />
           </div>
         </div>
@@ -28,15 +90,27 @@ const SellingProduct = () => {
           </div>
           <div className="lower px-5 pt-3">
             <div className="text-muted">Unit Price</div>
+            <div className='text-danger'>{formError.unitprice}</div>
             <Input
               className="w-50 border py-2 mt-3 mb-5 rounded-3 border-2"
               type="number"
+              name="unitprice"
+              onChange={handleChangeForm}
+              value={form.unitprice}
               placeholder="input in rupiah" />
             <div className="text-muted">Stock</div>
+            <div className='text-danger'>{formError.unitbuah}</div>
             <Input
               className="w-50 border py-2 mt-3 mb-5 rounded-3 border-2"
               type="number"
+              name="unitbuah"
+              onChange={handleChangeForm}
+              value={form.unitbuah}
               placeholder="input in buah" />
+              <div className="text-muted">Stock</div>
+              <div className='text-danger'>{formError.unitstock}</div>
+              <input type="radio" name="unitstock" onChange={handleChangeForm}  value={form.unitstock}   /> baru
+              <input type="radio" name="unitstock" onChange={handleChangeForm}  value={form.unitstock} className="ms-5 mb-5" /> bekas
           </div>
         </div>
       </div>
@@ -69,17 +143,21 @@ const SellingProduct = () => {
           <div className="upper px-5 pt-5">
             <div className="fw-bold">Description</div>
             <hr />
+            <div className='text-danger'>{formError.description}</div>
           </div>
           <div className="lower px-5 pt-3">
             <textarea
               type="text"
+              name="description"
+              onChange={handleChangeForm}
+              value={form.description}
               placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam eum, quis architecto mollitia, aliquam debitis quos iure modi fugiat dicta nostrum deserunt minus possimus praesentium exercitationem doloribus nisi, vero accusamus!"
               className="desc-box mb-5"
             />
           </div>
         </div>
       </div>
-      <div className="text-end me-5 mb-5"><Button className="rounded-pill px-5 py-3 text-white bg-danger">Jual</Button></div>
+      <div className="text-end me-5 mb-5"><Button onClick={handleSubmitForm} className="rounded-pill px-5 py-3 text-white bg-danger">Jual</Button></div>
     </div>
   )
 };
--- src/pages/main/Profile/StoreProfile/index.js
+++ src/pages/main/Profile/StoreProfile/index.js
@@ -1,8 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../profile.css'
 import Button from '../../../../components/base/Button';
 import Input from '../../../../components/base/Input';
 
 const StoreProfile = () => {
+  const [loading, setLoading] = useState(false)
+  const [formError, setFormError] = useState({})
+
+  const [form, setForm] = useState({
+    storename: '',
+    email: '',
+    phone: '',
+    storedesc: ''
+  })
+
+  const handleChangeForm = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: [e.target.value]
+    })
+  }
+
+  const validateForm = (values) => {
+    const errors = {};
+    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    const textStore = values.storedesc.toString()
+    
+    if (!values.storename) {
+      errors.storename = "Store name is required";
+    }
+    if (!values.email) {
+      errors.email = "Email is required";
+    } else if (!regex.test(values.email)) {
+      errors.email = "Email address is invalid";
+    }
+    if (!values.phone) {
+      errors.phone = "Phone number is required";
+    }
+    if (!values.storedesc) {
+      errors.storedesc = "Store description is required";
+    } else if (textStore.length < 120) {
+      errors.storedesc = "Store description atleast must be more than 120 characters";
+    }
+    return errors;
+  }
+  const handleClickForm = (resultValidate) => {
+    if (Object.keys(resultValidate).length === 0) {
+      setLoading(true)
+      console.log("dari form", form)
+      alert("oke")
+    }
+  }
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    const resultValidate = validateForm(form)
+    setFormError(resultValidate)
+    handleClickForm(resultValidate)
+    console.log("isi juga dari", form)
+  }
   return (
     <div className="outer bg-light w-75">
       <div className="inner m-5 bg-white rounded">
@@ -14,36 +70,55 @@ const StoreProfile = () => {
           </div>
           <div className="profile-lower d-flex justify-content-between px-5 pt-3">
             <div className="w-75">
-              <div className="d-flex mb-3 ms-5">
-                <div className="w-50 text-muted">Store Name</div>
+              <div className="row mb-3 ms-5">
+                <div className="col-3 text-muted">Store Name</div>
                 <Input
+                  className="col"
                   type="text"
+                  name="storename"
+                  onChange={handleChangeForm}
+                  value={form.name}
                   placeholder="Johannes Michael"
                 />
+                <div className="row validate-error text-danger">{formError.storename}</div>
               </div>
-              <div className="d-flex mb-3 ms-5">
-                <div className="w-50 text-muted">Email</div>
+              <div className="row mb-3 ms-5">
+                <div className="col-3 text-muted">Email</div>
                 <Input
+                  className="col"
                   type="email"
+                  name="email"
+                  onChange={handleChangeForm}
+                  value={form.email}
                   placeholder="johanes@gmail.com"
                 />
+                <div className="row validate-error text-danger">{formError.email}</div>
               </div>
-              <div className="d-flex mb-3 ms-5">
-                <div className="w-50 text-muted">Phone number</div>
+              <div className="row mb-3 ms-5">
+                <div className="col-3 text-muted">Phone number</div>
                 <Input
+                  className="col"
                   type="number"
+                  name="phone"
+                  onChange={handleChangeForm}
+                  value={form.phone}
                   placeholder="08901289012"
                 />
+                <div className="row validate-error text-danger">{formError.phone}</div>
               </div>
-              <div className="d-flex mb-5 ms-5">
-                <div className="w-50 text-muted">Store description</div>
+              <div className="row mb-5 ms-5">
+                <div className="col-3 mb-3 text-muted">Store description</div>
                 <textarea
+                  className="col desc-box"
                   type="text"
+                  name="storedesc"
+                  onChange={handleChangeForm}
+                  value={form.storedesc}
                   placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam eum, quis architecto mollitia, aliquam debitis quos iure modi fugiat dicta nostrum deserunt minus possimus praesentium exercitationem doloribus nisi, vero accusamus!"
-                  className="desc-box"
                 />
+                <div className="row validate-error text-danger">{formError.storedesc}</div>
               </div>
-              <Button className="bg-danger text-white rounded-pill px-5 py-2 mb-5">Save</Button>
+              <Button onClick={handleSubmitForm} className="bg-danger text-white rounded-pill px-5 py-2 mb-5">Save</Button>
             </div>
             <span className="flex-shrink-1 vertical-line" />
             <div className="flex-shrink-1">
